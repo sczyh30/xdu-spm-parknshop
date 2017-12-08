1b17ec697e74d75898a36d58198dcb927d291f70
Eric Zhao <sczyh16@gmail.com>
2017-12-08 22:18:51 +0800

    Update dependencies
    Signed-off-by: sczyh30 <sczyh16@gmail.com>

--- common/src/main/java/io/spm/parknshop/common/auth/AuthCenter.java
+++ common/src/main/java/io/spm/parknshop/common/auth/AuthCenter.java
@@ -1,10 +1,15 @@
 package io.spm.parknshop.common.auth;
 
+/**
+ * @author Eric Zhao
+ */
 public final class AuthCenter {
 
   public static String encryptDefault(String str) {
     return str;
   }
+
+
 
   private AuthCenter() {}
 }
